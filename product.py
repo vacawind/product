2766f8e3952809a1b14db4dac222ef17ccd7d615
JJ Lee <vacawind@gmail.com>
2020-10-11 22:03:48 +0800

    update 2維清單(商品與價格)

--- product.py
+++ product.py
@@ -4,11 +4,9 @@ while True:
 	name = input("輸入商品名稱(若要停止輸入請按q)：")
 	if name == "q":
 		break
-	price = input("輸入商品價格：")
-	p = []         #建立二維清單
-	p.append(name)
-	p.append(price)
-	products.append(p)
+	price = input("輸入商品價格：") 
+	p = [name, price] # 建立二維清單的進階寫法
+	products.append(p)   
 print(products)
 
 
