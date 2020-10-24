2debf98ece324b7323cdf8790f437e43869b68f9
JJ Lee <vacawind@gmail.com>
2020-10-24 20:26:03 +0800

    update refactor

--- product.py
+++ product.py
@@ -3,20 +3,16 @@ import os # operating system
 # 確認檔案是否存在
 def read_file(filename):
 	products = []
-	if os.path.isfile(filename):
-		print("找到檔案了!!!")
-		with open(filename, 'r', encoding = "utf-8") as f:
-			for line in f:
-				if "商品名稱,商品價格" in line:
-					continue
-				name, price = line.strip().split(",")
-				# s = line.strip().split(",")
-				# name = s[0]
-				# price = s[1]
-				products.append([name,price])
-			print(products)
-	else:
-		print("找不到檔案~~")
+	with open(filename, 'r', encoding = "utf-8") as f:
+		for line in f:
+			if "商品名稱,商品價格" in line:
+				continue
+			name, price = line.strip().split(",")
+			# s = line.strip().split(",")
+			# name = s[0]
+			# price = s[1]
+			products.append([name,price])
+		print(products)
 	return products
 
 
@@ -38,6 +34,7 @@ def print_products(products):
 	for p in products:
 		print(p[0], "的價格是", p[1])
 
+
 # 寫入檔案
 def write_file(filename, products):
 	with open("product.csv", 'w', encoding = "utf-8") as f:  #寫入中文時編碼，但若用excel開啟時還需從data在選編碼
@@ -45,13 +42,23 @@ def write_file(filename, products):
 		for p in products:
 			f.write(p[0] + "," + p[1] + "\n")
 
+
+# 主程式
+def main(filename):
+	if os.path.isfile(filename):
+		print("找到檔案了!!!")
+		products = read_file(filename)
+	else:
+		print("找不到檔案~~")
+
+	products = user_input(products)
+
+	print_products(products)
+
+	write_file(filename, products)
+	
+
+
 filename = "product.csv"
-products = read_file(filename)
-
-products = user_input(products)
-
-print_products(products)
-
-write_file(filename, products)
-
-
+# 主程式進入點
+main(filename)
